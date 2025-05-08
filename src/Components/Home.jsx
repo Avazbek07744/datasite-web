e0a6cebe1f3d918e21ad472ed16e05e805551ff2
lord2202009 <azizbekabdurahimov2209@gmail.com>
2025-05-08 13:56:08 +0500

    carusel

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ import {
     CarouselItem,
     CarouselNext,
     CarouselPrevious,
-} from "@/components/ui/carousel";
+} from "../components/ui/carousel";
 import CountUp from "react-countup";
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
